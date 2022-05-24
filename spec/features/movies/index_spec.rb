424abeaa7ef2bd2d4ebd540f0bdde7475c5e44ca
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-24 15:04:30 -0600

    Refactor: movies/index_spec.rb passing tests after route change

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -5,14 +5,20 @@ require 'rails_helper'
 describe 'The movie results page' do
   before do
     @user = User.create!(name: 'Brylan', email: 'brylan.gannon112@gmail.com', password: 'brylan', password_confirmation: 'brylan')
-    visit "/users/#{@user.id}/discover"
+    visit '/'
+    click_on 'Login'
+    fill_in :email, with: @user.email
+    fill_in :password, with: @user.password
+    click_button 'Login'
+
+    visit "/dashboard/discover"
   end
 
   context 'after clicking the Top Rated button' do
     it 'displays the top 40 movies' do
       click_button 'Top Rated Movies'
 
-      expect(current_path).to eq(user_movies_path(@user))
+      expect(current_path).to eq('/dashboard/movies')
       expect(page).to have_css('#movie', count: 40)
     end
   end
